f4a349e1fa106b8bfffb985c1b0bf54c8bfd777a
JoseHenrique1 <jh4832184@gmail.com>
2025-04-30 10:12:14 -0300

    view: add task-card

--- src/app.tsx
+++ src/app.tsx
@@ -1,23 +1,29 @@
+import { Plus } from "lucide-react"
 import { Filter } from "./components/filter"
 import { Button } from "./components/ui/button"
 import { Input } from "./components/ui/input"
+import { Header } from "./components/header"
+import { TaskCard } from "./components/task-card"
 
 function App() {
+  const a = [1,2,3]
   return (
     <div>
-      <header>TASK MANAGER</header>
+      <Header />
       <main>
-        <Input placeholder="Serach task" />
-        <Filter />
+        <div className="flex gap-2 px-4 py-0">
+          <Input placeholder="Search task" />
+          <Filter />
+          <Button><Plus /></Button>
+        </div>
 
-        <div>
-          <div>task01</div>
-          <div>task01</div>
-          <div>task01</div>
-          <div>task01</div>
+        <div className="p-4 space-y-1">
+          {a.map((_, index) => (
+            <TaskCard key={index} />
+          ))}
+          
         </div>
       </main>
-      <Button>hahahah</Button>
     </div>
   )
 }
---
+++ src/components/header.tsx
@@ -0,0 +1,5 @@
+export function Header() {
+  return (
+    <header className="p-4">TASK MANAGER</header>
+  )
+}
